300bded3111531b0b944d3102ccec53b384a2aaf
오유원 <nuil@29cm.co.kr>
2020-02-16 22:21:43 +0900

    로그인 페이지 레이아웃 구성

---
+++ app/src/main/java/kr/hanlight/login/LoginActivity.kt
@@ -0,0 +1,13 @@
+package kr.hanlight.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kr.hanlight.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
